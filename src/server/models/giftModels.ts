a9384185c4603038df145e31004b294536e2e173
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-11-16 17:42:44 -0600

    Set up Gift System routes

--- src/server/models/giftModels.ts
+++ src/server/models/giftModels.ts
@@ -22,6 +22,42 @@ const createWishListsQuery = {
   params: []
 }
 
+const getWishList = {
+  // TODO: Complete and test this query
+  // select label, description, link, and status from 'gifts'
+  // join on user_id, gift_id
+  // where user_id = userID
+  //** Find all the gifst in 'wish_lists where user_id = userID */
+  // text: `SELECT 
+  //   gifts.label, gifts.description, gifts.link, gifts.status, wish_lists.user_id
+  //   FROM gifts INNER JOIN wish_lists ON (gifts._id = wish_lists.gift_id)
+  //   WHERE wish_lists.user_id = ${userID};
+  // `
+}
+
+export const createGift = (label, description, link) => {
+  const query = {
+  text: `INSERT INTO gifts(label, description, link)
+  VALUES ($1, $2, $3)
+  RETURNING _id`,
+  params: [label, description, link]
+  };
+  db.query(query, (dbResponse) => {
+    return dbResponse.rows;
+  })
+}
+
+export const addGiftToWishList = (userID, giftID) => {
+  const query = {
+    text: `INSERT INTO wish_lists(user_id, gift_id)
+    VALUES ($1, $2)`,
+    params: [userID, giftID]
+  };
+  db.query(query, dbResponse) => {
+    return dbResponse.rows;
+  }
+}
+
 // queries for shopping_lists table
 const createShoppingListsQuery = {
   text: `CREATE TABLE shopping_lists(
