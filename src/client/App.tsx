ec5ec257538700eb973964022f4f92c097db615c
Miguel Hernandez <miguelh72@outlook.com>
2021-11-16 12:12:57 -0500

    skeleton client components and how page navigation works
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

---
+++ src/client/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import { Page } from './types';
+import SignIn from './login/Login';
+import SignUp from './signup/SignUp';
+import LoggedIn from './common/LoggedIn';
+
+
+export default function App() {
+  const [page, setPage] = useState<Page>(Page.Login);
+
+  return (<>
+    {page === Page.Login && <SignIn />}
+    {page === Page.SignUp && <SignUp />}
+    {page !== Page.Login && page !== Page.SignUp && <LoggedIn page={page} />}
+  </>);
+}
