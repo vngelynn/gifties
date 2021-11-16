4b2a09950b278803a04dd60e68d1251dcc2af733
angelynn <angelynn.trng@gmail.com>
2021-11-16 15:01:14 -0800

    Working on testing.

--- __tests__/src/client/redux/wishlistSlice.test.ts
+++ __tests__/src/client/redux/wishlistSlice.test.ts
@@ -12,16 +12,37 @@ describe('Test wishlist reducer and action creators', () => {
     link: 'www.nocook.com',
     status: 'available',
   };
+  
 
   beforeEach(() => {
     const combinedReducer = combineReducers({
         wishlist: wishlistReducer,
     });
+
     store = createStore(combinedReducer);
   });
 
-//   test('Adds a new wish to wishlist', () => {
+  test('Test setting wishlist state', () => {
+    store.dispatch(setWishList(giftItem));
+    expect(store.getState().wishlist).toEqual(giftItem);
+  });
 
-//   });
+  test('Adds a new wish to wishlist', () => {
+    const length = store.getState().wishlist.length;
+
+    const giftItem = {
+        id: 302,
+        label: 'Hot Wheels',
+        description: 'red',
+        link: 'www.cars.com',
+        status: 'available',
+    };
+    store.dispatch(addWish(giftItem));
+    expect(store.getState().wishlist.length).toEqual(length+1);
+  });
+
+  test('Deletes a book from list with id', () => {
+    store.dispatch(deleteWish);
+  });
 
 });
