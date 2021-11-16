2b3de7caa87741d6a9f47abbc867cbafc6c94edf
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-11-16 14:40:18 -0600

    Updated Create Table queries to be used in functions

--- src/server/models/giftModels.ts
+++ src/server/models/giftModels.ts
@@ -1,22 +1,33 @@
+const db = require('../models/db');
+
 // queries for gifts table
-const createGiftsTable = `CREATE TABLE gifts(
+const createGiftsQuery = {
+  text: `CREATE TABLE gifts(
   _id SERIAL PRIMARY KEY,
   label VARCHAR NOT NULL,
   description VARCHAR,
   link VARCHAR,
   status VARCHAR NOT NULL DEFAULT 'available'
-  );`
+  );`,
+  params: []
+}
 
 // queries for wish_lists table
-const createWishListsTable = `CREATE TABLE wish_lists(
+const createWishListsQuery = {
+  text: `CREATE TABLE wish_lists(
   _id SERIAL PRIMARY KEY,
   CONSTRAINT gift_id FOREIGN KEY (_id) REFERENCES gifts(_id),
   CONSTRAINT user_id FOREIGN KEY (_id) REFERENCES users(_id)
-  );`
+  );`,
+  params: []
+}
 
 // queries for shopping_lists table
-const createShoppingListsTable = `CREATE TABLE shopping_lists(
+const createShoppingListsQuery = {
+  text: `CREATE TABLE shopping_lists(
   _id SERIAL PRIMARY KEY,
   CONSTRAINT gift_id FOREIGN KEY (_id) REFERENCES gifts(_id),
   CONSTRAINT user_id FOREIGN KEY (_id) REFERENCES users(_id)
-  );`
+  );`,
+  params: []
+}
--- src/server/models/userModels.ts
+++ src/server/models/userModels.ts
@@ -1,29 +1,41 @@
 // queries for users table
-const createUserTable = `CREATE TABLE users(
+const createUserQuery = {
+  text: `CREATE TABLE users(
   _id SERIAL PRIMARY KEY,
   display_name VARCHAR NOT NULL,
   email VARCHAR NOT NULL UNIQUE,
   passhash VARCHAR NOT NULL,
   profile_image VARCHAR NOT NULL,
-  );`
+  );`,
+  params: []
+}
 
 // queries for besties table
-const createBestiesTable = `CREATE TABLE besties(
+const createBestiesQuery = {
+  text: `CREATE TABLE besties(
   _id SERIAL PRIMARY KEY,
   CONSTRAINT user1_id FOREIGN KEY (_id) REFERENCES users(_id),
   CONSTRAINT user2_id FOREIGN KEY (_id) REFERENCES users(_id),
   status VARCHAR NOT NULL DEFAULT 'pending'
-);`
+  );`,
+  params: []
+}
 
 //queries for questions and question_answers tables
-const createQuestionsTable = `CREATE TABLE questions(
+const createQuestionsQuery = {
+  text: `CREATE TABLE questions(
   _id SERIAL PRIMARY KEY,
   question VARCHAR NOT NULL
-  );`
+  );`,
+  params: []
+}
 
-const createQuestionAnswersTable = `CREATE TABLE question_answers(
+const createQuestionAnswersTable = {
+  text: `CREATE TABLE question_answers(
   _id SERIAL PRIMARY KEY,
   CONSTRAINT user_id FOREIGN KEY (_id) REFERENCES users(_id),
   CONSTRAINT question_id FOREIGN KEY (_id) REFERENCES questions(_id),
   answer VARCHAR NOT NULL
-  );`
+  );`,
+  param: []
+}
