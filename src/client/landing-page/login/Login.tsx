a2b4d5ffaf24bb8a1863387027cf5df8194c7ebf
Angelynn <89512890+vngelynn@users.noreply.github.com>
2024-10-29 01:49:56 -0700

    add labels to login/signup forms

--- src/client/landing-page/login/Login.tsx
+++ src/client/landing-page/login/Login.tsx
@@ -1,58 +1,68 @@
-import React, { useState, useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useCallback } from "react"
+import { useDispatch } from "react-redux"
 
-import useField from './../../hooks/useField';
-import { Page } from './../../types';
-import { loginRequest } from './../../utils/fetch';
-import { setPage } from './../../redux/pageSlice';
-import { setUser } from './../../redux/userSlice';
-import { setBesties } from './../../redux/bestiesSlice';
-import { setShoppingList } from './../../redux/shoppingListSlice';
-import { setWishList } from './../../redux/wishListSlice';
+import useField from "./../../hooks/useField"
+import { Page } from "./../../types"
+import { loginRequest } from "./../../utils/fetch"
+import { setPage } from "./../../redux/pageSlice"
+import { setUser } from "./../../redux/userSlice"
+import { setBesties } from "./../../redux/bestiesSlice"
+import { setShoppingList } from "./../../redux/shoppingListSlice"
+import { setWishList } from "./../../redux/wishListSlice"
 
-import './Login.scss';
+import "./Login.scss"
 
 export default function Login({
   showSignUpPage,
-  // showPasswordRecoveryPage
-}: {
-  showSignUpPage: () => void;
+}: // showPasswordRecoveryPage
+{
+  showSignUpPage: () => void
   // showPasswordRecoveryPage: () => void;
 }) {
-  const [email, onEmailChange] = useField('');
-  const [password, onPasswordChange] = useField('');
-  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [email, onEmailChange] = useField("")
+  const [password, onPasswordChange] = useField("")
+  const [errorMessage, setErrorMessage] = useState<string>("")
   // const [wasFailedAttemptMade, setWasFailedAttemptMade] = useState<boolean>(false); // TODO
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const attemptLogin = useCallback(async () => {
     // TODO validate input fields before making server request
-    const appState = await loginRequest(email, password);
+    const appState = await loginRequest(email, password)
 
     if (appState === undefined) {
-      setErrorMessage('Invalid credentials, please try again.');
+      setErrorMessage("Invalid credentials, please try again.")
     } else {
       // ! is this the correct way to do this? I feel like this should be combined into a single reducer all
-      dispatch(setUser(appState.user));
-      dispatch(setBesties(appState.besties));
-      dispatch(setShoppingList(appState.shoppingList));
-      dispatch(setWishList(appState.wishList));
-      dispatch(setPage(Page.WishList));
+      dispatch(setUser(appState.user))
+      dispatch(setBesties(appState.besties))
+      dispatch(setShoppingList(appState.shoppingList))
+      dispatch(setWishList(appState.wishList))
+      dispatch(setPage(Page.WishList))
     }
-  }, [dispatch, email, password]);
+  }, [dispatch, email, password])
 
-  return (<>
-    <h1>LOGIN</h1>
-    <div id='login-form'>
-      {errorMessage.length > 0 && <p className='error-message'>{errorMessage}</p>}
-      <input type='email' value={email} onChange={onEmailChange} />
-      <input type='password' value={password} onChange={onPasswordChange} />
-      <button type='submit' onClick={attemptLogin}>LOGIN</button>
-      <button type='submit' onClick={showSignUpPage}>SIGN UP</button>
-      { /*wasFailedAttemptMade && <div>
+  return (
+    <>
+      <h1>LOGIN</h1>
+      <div id='login-form'>
+        {errorMessage.length > 0 && (
+          <p className='error-message'>{errorMessage}</p>
+        )}
+        <label>email</label>
+        <input type='email' value={email} onChange={onEmailChange} />
+        <label>password</label>
+        <input type='password' value={password} onChange={onPasswordChange} />
+        <button type='submit' onClick={attemptLogin}>
+          LOGIN
+        </button>
+        <button type='submit' onClick={showSignUpPage}>
+          SIGN UP
+        </button>
+        {/*wasFailedAttemptMade && <div>
         <a id='password-recovery' onClick={showPasswordRecoveryPage}>Forgot password?</a>
-      </div> */ }
-    </div>
-  </>);
+      </div> */}
+      </div>
+    </>
+  )
 }
--- src/client/landing-page/signup/SignUp.tsx
+++ src/client/landing-page/signup/SignUp.tsx
@@ -1,52 +1,61 @@
-import React, { useState, useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useCallback } from "react"
+import { useDispatch } from "react-redux"
 
-import useField from './../../hooks/useField';
-import { Page, ErrorMessage } from './../../types';
-import { AppState } from 'redux/store.ts';
-import { createAccountRequest } from './../../utils/fetch';
-import { setPage } from './../../redux/pageSlice';
-import { setUser } from './../../redux/userSlice';
-import { setBesties } from './../../redux/bestiesSlice';
-import { setShoppingList } from './../../redux/shoppingListSlice';
-import { setWishList } from './../../redux/wishListSlice';
+import useField from "./../../hooks/useField"
+import { Page, ErrorMessage } from "./../../types"
+import { AppState } from "redux/store.ts"
+import { createAccountRequest } from "./../../utils/fetch"
+import { setPage } from "./../../redux/pageSlice"
+import { setUser } from "./../../redux/userSlice"
+import { setBesties } from "./../../redux/bestiesSlice"
+import { setShoppingList } from "./../../redux/shoppingListSlice"
+import { setWishList } from "./../../redux/wishListSlice"
 
-import './SignUp.scss';
+import "./SignUp.scss"
 
 export default function SignUp() {
-  const [name, onNameChange] = useField('');
-  const [email, onEmailChange] = useField('');
-  const [password, onPasswordChange] = useField('');
-  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [name, onNameChange] = useField("")
+  const [email, onEmailChange] = useField("")
+  const [password, onPasswordChange] = useField("")
+  const [errorMessage, setErrorMessage] = useState<string>("")
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const attemptCreateAccount = useCallback(async () => {
     // TODO validate input fields before making server request
-    const serverResponse = await createAccountRequest(name, email, password);
+    const serverResponse = await createAccountRequest(name, email, password)
 
     if ((serverResponse as ErrorMessage).error !== undefined) {
-      setErrorMessage((serverResponse as ErrorMessage).error);
+      setErrorMessage((serverResponse as ErrorMessage).error)
     } else {
       // ! is this the correct way to do this? I feel like this should be combined into a single reducer all
-      const appState: AppState = serverResponse as AppState;
-      dispatch(setUser(appState.user));
-      dispatch(setBesties(appState.besties));
-      dispatch(setShoppingList(appState.shoppingList));
-      dispatch(setWishList(appState.wishList));
-      dispatch(setPage(Page.WishList));
+      const appState: AppState = serverResponse as AppState
+      dispatch(setUser(appState.user))
+      dispatch(setBesties(appState.besties))
+      dispatch(setShoppingList(appState.shoppingList))
+      dispatch(setWishList(appState.wishList))
+      dispatch(setPage(Page.WishList))
     }
-  }, [dispatch, email, name, password]);
+  }, [dispatch, email, name, password])
 
-  return (<>
-    <h1>Sign Up</h1>
-    <div id='signup-form'>
-      {errorMessage.length > 0 && <p className='error-message'>{errorMessage}</p>}
-      <input type='text' value={name} onChange={onNameChange} />
-      <input type='email' value={email} onChange={onEmailChange} />
-      <input type='password' value={password} onChange={onPasswordChange} />
-      {/* TODO handle profile image selection and security questions */}
-      <button type='submit' onClick={attemptCreateAccount}>Create Account</button>
-    </div>
-  </>);
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <div id='signup-form'>
+        {errorMessage.length > 0 && (
+          <p className='error-message'>{errorMessage}</p>
+        )}
+        <label>username</label>
+        <input type='text' value={name} onChange={onNameChange} />
+        <label>email</label>
+        <input type='email' value={email} onChange={onEmailChange} />
+        <label>password</label>
+        <input type='password' value={password} onChange={onPasswordChange} />
+        {/* TODO handle profile image selection and security questions */}
+        <button type='submit' onClick={attemptCreateAccount}>
+          Create Account
+        </button>
+      </div>
+    </>
+  )
 }
